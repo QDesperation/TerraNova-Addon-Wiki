85988b3015696f6c91843b4032d8b33112efdccd
Rezn1r <rezn1r@proton.me>
2026-01-23 11:55:46 +0800

    feat: Add Ammonite and Doedicurus mob pages with icons and info boxes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: 'https://qdesperation.github.io/',
   base: '/TerraNova-Addon-Wiki/',
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  integrations: [mdx()]
 });
